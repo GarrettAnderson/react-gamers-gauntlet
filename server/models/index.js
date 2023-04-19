6a2cc8485249997ea32e6c31c3420b2523e5c2c8
Garrett <garrettleegrahamanderson@gmail.com>
2023-04-18 21:20:13 -0400

    updated the mongo db connection file with the gamers gauntlet db name. created a score model and added that to the index.js

--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,8 @@
-const User = require('./User');
-const Product = require('./Product');
-const Category = require('./Category');
-const Order = require('./Order');
+const User = require("./User");
+const Score = require("./Score");
+
+const Product = require("./Product");
+const Category = require("./Category");
+const Order = require("./Order");
 
 module.exports = { User, Product, Category, Order };
