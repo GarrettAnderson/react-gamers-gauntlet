32674e5a4849bc1a9d1616ee31bafb0adf091380
Thomas <tjb47.dev@gmail.com>
2023-04-28 20:13:12 -0400

    refactored login.js, styled login.css. Added image to the background

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import mernLogo from '../assets/mern-logo.png'
+
 
 function Header() {
-
     return (
-        <header className='d-flex'>
+        <header className='d-flex' style={{ backgroundColor: 'transparent' }}>
             <div className='d-flex row'>
-                <div className='logo-div mx-3 m-1'>
+                {/* <div className='logo-div mx-3 m-1'>
                     <img className='mern-logo' src={mernLogo} alt='MERN Logo'></img>
-                </div>
+                </div> */}
                 <div className='mern-trivia'>
-                    <h1 className='m-0'>MERN Trivia</h1>
+                    <h1 className='m-0'>Gamers Gauntlet</h1>
                 </div>
             </div>
         </header>
